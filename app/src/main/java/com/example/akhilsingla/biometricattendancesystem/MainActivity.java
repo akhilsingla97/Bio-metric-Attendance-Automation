51508e86cc9f2f96fda560ec4b0dd2cd3405313e
ma5terdrag0n <prince.thakkar001@gmail.com>
2017-10-07 00:44:46 -0700

    initiated

--- app/src/main/java/com/example/akhilsingla/biometricattendancesystem/MainActivity.java
+++ app/src/main/java/com/example/akhilsingla/biometricattendancesystem/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.akhilsingla.biometricattendancesystem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void signin(View v){
+        Intent intent = new Intent(this, signin_activity.class);
+        startActivity(intent);
+    }
+    public void signup(View v){
+        Intent intent = new Intent(this, signup_activity.class);
+        startActivity(intent);
     }
 }
