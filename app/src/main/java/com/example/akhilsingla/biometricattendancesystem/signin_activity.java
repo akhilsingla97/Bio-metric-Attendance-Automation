b1cc4117fc1c3132471f5f106c306729c7816d74
ma5terdrag0n <prince.thakkar001@gmail.com>
2017-10-07 04:18:23 -0700

    Database connected and login and sign up successfull

--- app/src/main/java/com/example/akhilsingla/biometricattendancesystem/signin_activity.java
+++ app/src/main/java/com/example/akhilsingla/biometricattendancesystem/signin_activity.java
@@ -1,13 +1,82 @@
 package com.example.akhilsingla.biometricattendancesystem;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class signin_activity extends AppCompatActivity {
-
+    Button login;
+    private EditText number, password;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin_activity);
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        number = (EditText)findViewById(R.id.editText);
+        password = (EditText)findViewById(R.id.editText2);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        login= (Button) findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                validate_login();
+
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;}
+        return true;
+    }
+    void validate_login()
+    {
+        try{
+            mDatabase.child("users").child(number.getText().toString()).child("passw").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String pass = dataSnapshot.getValue(String.class);
+                    if (pass!=null&&pass.equals(password.getText().toString())) {
+                        Toast.makeText(getApplicationContext(), "Login Successfull", Toast.LENGTH_SHORT).show();
+//                        Intent i= new Intent(getApplicationContext(),home_activity.class);
+//                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                        startActivity(i);
+
+                    }
+
+                    else
+                        Toast.makeText(getApplicationContext(),"Login Failed",Toast.LENGTH_SHORT).show();
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/akhilsingla/biometricattendancesystem/signup_activity.java
+++ app/src/main/java/com/example/akhilsingla/biometricattendancesystem/signup_activity.java
@@ -1,13 +1,93 @@
 package com.example.akhilsingla.biometricattendancesystem;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class signup_activity extends AppCompatActivity {
-
+    Button b1;
+    private EditText eid, password, confpassword, phone;
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_activity);
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        eid = (EditText)findViewById(R.id.editText);
+        password = (EditText)findViewById(R.id.editText2);
+        confpassword = (EditText)findViewById(R.id.editText3);
+        phone = (EditText)findViewById(R.id.editText4);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        b1= (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    insertData();
+                }catch(Exception e){
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean phoneCheck(){
+        String s = phone.getText().toString();
+        return (s.length() == 10);
+    }
+
+    public void insertData(){
+        try{
+            if(!phoneCheck()){
+                Toast.makeText(getApplicationContext(), "Enter correct phone number", Toast.LENGTH_LONG).show();
+                return;
+            }
+            String testingVariable =  password.getText().toString();
+            if(testingVariable.length() < 6){
+                Toast.makeText(getApplicationContext(), "Password must be atleast 6 characters long", Toast.LENGTH_LONG).show();
+                return;
+            }
+            if(!testingVariable.equals(confpassword.getText().toString())){
+                Toast.makeText(getApplicationContext(), "Enter same password in confirm password", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+
+            mDatabase.child("users").child(phone.getText().toString()).child("eid").setValue(eid.getText().toString());
+            mDatabase.child("users").child(phone.getText().toString()).child("passw").setValue(password.getText().toString());
+            Toast.makeText(getApplicationContext(), "Account created Successfully !", Toast.LENGTH_SHORT).show();
+            finish();
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 }
