57f0aa983865709265cbe4c52dd7cff39cc03cc5
Akhil Singla <akhilsingla97@gmail.com>
2017-11-16 02:51:47 +0530

    final changes

---
+++ app/src/main/java/com/example/akhilsingla/biometricattendancesystem/Admin_signin.java
@@ -0,0 +1,13 @@
+package com.example.akhilsingla.biometricattendancesystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Admin_signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_signin);
+    }
+}
